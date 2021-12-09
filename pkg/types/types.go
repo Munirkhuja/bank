d75e91f55eaff95b69d400480f37eda409740ce7
munirhuja1357@gmail.com <bit314151>
2021-12-09 10:18:47 +0500

    v2 commit first

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -19,10 +19,17 @@ type Card struct {
 	Active bool
 }
 type Category string
+type Status string
+const (
+	StatusOk Status="OK"
+	StatusFail Status="FAIL"
+	StatusInProgress Status="INPROGRESS"
+)
 type Payment struct {
 	ID uint
 	Amount Money
 	Category Category
+	Status Status
 }
 type PaymentSource struct {
 	Type string // 'card'
